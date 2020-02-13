9bb67efb186312fdc2da14b22205d886e96d3a85
OuSS <ouss.cs@gmail.com>
2020-02-13 18:23:15 +0100

    Initial Commit

---
+++ AppstoreJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppstoreJSON
+//
+//  Created by OuSS on 2/13/20.
+//  Copyright © 2020 OuSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
