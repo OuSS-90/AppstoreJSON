3c7f4dd37bbe4b0afdfd97e1e5b1fea78d9c209a
OuSS <ouss.cs@gmail.com>
2020-02-13 18:47:08 +0100

    Setup project

---
+++ AppstoreJSON/BaseTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  BaseTabBarController.swift
+//  AppstoreJSON
+//
+//  Created by OuSS on 2/13/20.
+//  Copyright © 2020 OuSS. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon"),
+            createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+            createNavController(viewController: UIViewController(), title: "Search", imageName: "search")
+        ]
+    }
+    
+    private func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        viewController.view.backgroundColor = .white
+        viewController.navigationItem.title = title
+        return navController
+    }
+}
